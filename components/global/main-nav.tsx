f8f41e7860d965ec1e503532e650653a36e73c95
UrKo <urko2376@googlemail.com>
2025-07-15 16:00:56 +0100

    Amended signout to remove the 3 second delay

--- components/global/main-nav.tsx
+++ components/global/main-nav.tsx
@@ -27,8 +27,10 @@ useEffect(() => {
 
   if (status === "unauthenticated") {
     console.log("User signed out");
-    const timer = setTimeout(() => router.push("/"), 3000);
-    return () => clearTimeout(timer); // cleanup timer on unmount or session change
+    // Set time out code to render the sign out after 3 seconds
+    //const timer = setTimeout(() => router.push("/"), 3000);
+    //return () => clearTimeout(timer); // cleanup timer on unmount or session change
+    router.push("/");
   }
 
   if (status === "authenticated") {
